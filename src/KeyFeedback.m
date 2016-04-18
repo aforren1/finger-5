29772c032ece116b30659bccad1f10be523e26e8
Alex Forrence <aforren1@jhu.edu>
2016-04-18 12:12:08 -0400

    Tweaked feedback

--- src/KeyFeedback.m
+++ src/KeyFeedback.m
@@ -13,28 +13,25 @@ classdef KeyFeedback
     methods
         function obj = KeyFeedback(dims_x, dims_y, varargin)
         
-            obj.x = dims_x;
-            obj.y = dims_y;
-            
             opts = struct('n_boxes', 4,...
-                          'outline_colour' = [255, 255, 255],...
+                          'outline_colour', [255, 255, 255],...
                           'red', [255, 30, 63],...
                           'green', [97, 255, 77],...
                           'blue', [85, 98, 255]);
             opts = CheckInputs(opts, varargin{:});
-            obj.n_boxes = opt.n_boxes;
-            obj.outline_colour = opt.outline_colour;
-            obj.red = opt.red;
-            obj.green = opt.green;
-            obj.blue = opt.blue;
+            obj.n_boxes = opts.n_boxes;
+            obj.outline_colour = opts.outline_colour;
+            obj.red = opts.red;
+            obj.green = opts.green;
+            obj.blue = opts.blue;
             
             spacing = linspace(0.2, 0.8, obj.n_boxes);
             rect_area = (spacing(2) - spacing(1))/2;
-            base_rect = rect_area * [0 0 obj.x obj.x];
-            xrectpos = spacing * obj.x;
-            yrectpos = 0.8 * obj.y * ones(1, length(spacing));
+            base_rect = rect_area * [0 0 dims_x dims_x];
+            xrectpos = spacing * dims_x;
+            yrectpos = 0.8 * dims_y * ones(1, length(spacing));
 
-            obj.rect_locs = nan(4, n_finger);
+            obj.rect_locs = nan(4, opts.n_boxes);
             for pp = 1:length(yrectpos)
                 obj.rect_locs(:,pp) = CenterRectOnPointd(base_rect,...
                 xrectpos(pp), yrectpos(pp));
@@ -42,14 +39,20 @@ classdef KeyFeedback
         end % end constructor
         
         function DrawOutline(obj, scrn)
+            if ~isa(scrn, 'PsychScreen') 
+                error('Wrong class')
+            end
             Screen('FrameRect', scrn.window,...
                    obj.outline_colour, obj.rect_locs);
         end
         
         function DrawFill(obj, scrn, colour, indices)
+            if ~isa(scrn, 'PsychScreen')
+                error('Wrong class')
+            end
             which_colour = getfield(obj, colour);
             Screen('FillRect', scrn.window,...
-                   which_colour, obj.rect_locs(:, find(indices > 0));
+                   which_colour, obj.rect_locs(:, find(indices > 0)));
         end
     end % end methods
 end % end classdef
