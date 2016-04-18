c8dce7914447ee5214f59d5d1cd60d4cfe20e17a
Alex Forrence <aforren1@jhu.edu>
2016-04-18 09:50:32 -0400

    First pass at classes, leaving the resp dev for later because I don't quite get the super/subclass initialization yet

--- src/KeyResponse.m
+++ src/KeyResponse.m
@@ -1,20 +1,19 @@
-
-function output = KeyResponse(type, valid_indices, varargin)
-% type is the type of response device (keyboard or force transducer)
-% valid_indices are determined from the tgt file, and are used to specify
-%     which of the keys or force transducers should be used
-% varargin are additional constants set in the Constants.m file, or alternatively,
-%     are passed manually. (How to specify the order though??)
-
-if nargin < 2 % check for too few inputs
-    error('Too few inputs to KeyResponse().');
-end
-
-opts = struct('firstparameter', 1, 'secondparameter', magic(3));
-opts = CheckInputs(opts, varargin{:}); % this pattern ought to be pretty constant
-
-
-
-
-
+% superclass for keyboard-like response objects
+classdef KeyResponse
+    properties
+        valid_indices; 
+        press_tolerance; % how close to a beep counts as good
+        p1;
+    end
+    
+    methods
+        function obj = KeyResponse(valid_indices, press_tolerance,...
+                                   varargin)
+            opts = struct('p1', 33);
+            opts = CheckInputs(opts, varargin{:});
+            obj.p1 = ops.p1;
+            obj.valid_indices = valid_indices;
+            obj.press_tolerance = press_tolerance;
+        end
+    end
 end
