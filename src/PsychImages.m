78e6d77f497ba27c921a57d9280a1b4aa70b92cb
Alex Forrence <aforren1@jhu.edu>
2016-04-19 10:51:08 -0400

    Soften requirement of PsychScreen obj for PsychImages

--- src/PsychImages.m
+++ src/PsychImages.m
@@ -1,6 +1,6 @@
 classdef PsychImages
     properties
-        raw_images; % cell array of raw images 
+        raw_images; % 1xn cell array of raw images 
         ptb_images; % 1xn matrix of psychtoolbox handles
         reversed;
         scale;
@@ -18,16 +18,16 @@ classdef PsychImages
             obj.ptb_images = nan(1, n_images);
         end
         
-        function obj = ImportImages(obj, file_path, idx, scrn)
+        function obj = ImportImages(obj, file_path, idx, scrn_handle, scrn_dims_x)
             
             obj.raw_images{idx} = imread(file_path);
             tempimg = imresize(obj.raw_images{idx},...
-                               [obj.scale*scrn.dims(3) NaN]);
+                               [obj.scale*scrn_dims_x NaN]);
             if ~obj.reversed
                 tempimg = imcomplement(tempimg);
             end
             
-            obj.ptb_images(idx) = Screen('MakeTexture', scrn.window, tempimg);  
+            obj.ptb_images(idx) = Screen('MakeTexture', scrn_handle, tempimg);  
         end
     end
 end
