a06a27f85aa7b070668f778242cd6254ccacdb2a
Alex Forrence <aforren1@jhu.edu>
2016-04-28 13:10:48 -0400

    Skeleton force checking

--- src/ForceResponse.m
+++ src/ForceResponse.m
@@ -1,4 +1,4 @@
-classdef ForceResponse < KeyResponse
+classdef ForceResponse
     properties
         sampling_freq;
         calibration;
@@ -6,4 +6,24 @@ classdef ForceResponse < KeyResponse
         force_min; % in newtons
         force_max;
     end
-end
+    
+    methods
+        function obj = ForceResponse(valid_indices, varargin)
+        
+        end
+        
+        function StartKeyResponse(obj)
+        end
+        
+        function StopKeyResponse(obj)
+        end
+        
+        function DeleteKeyResponse(obj)
+        end
+        
+        function [new_press, updated_screen_press] = CheckKeyResponse(obj, updated_screen_press)
+        end
+        
+    end % end methods
+    
+end % end classdef
