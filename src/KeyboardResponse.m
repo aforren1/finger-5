0dbe5fc638d1d256e07ada8883e9a8c9e52fafe5
Alex Forrence <aforren1@jhu.edu>
2016-04-28 12:12:15 -0400

    First part of keyboard response

--- src/KeyboardResponse.m
+++ src/KeyboardResponse.m
@@ -1,25 +1,38 @@
-classdef KeyboardResponse < KeyResponse
+classdef KeyboardResponse
     properties
-       valid_keys;
-       possible_keys;
+        timing_tolerance;
+        valid_indices;
+        valid_keys;
+        force_min;
+        force_max;
     end
     
     methods
-    function obj = KeyboardResponse(valid_indices, varargin)
-        if ~exist('valid_indices', 'var')
-            error('Need valid indices!');
+        function obj = KeyboardResponse(valid_indices, varargin)
+            if ~exist('valid_indices', 'var')
+                error('Need valid indices!');
+            end
+            
+            opts = struct('possible_keys', {{'a','w','e','f','v','b','h','u','i','l'}},...
+                          'timing_tolerance', 0.075,...
+                          'force_min', 1,...
+                          'force_max', 100);
+            opts = CheckInputs(opts, varargin{:});
+            obj.timing_tolerance = opts.timing_tolerance;
+            obj.force_min = opts.force_min;
+            obj.force_max = opts.force_max;
+            obj.valid_keys = opts.possible_keys(valid_indices);
+            keys = zeros(1, 256);
+            keys(KbName(obj.valid_keys)) = 1;
+            KbQueueCreate(-1, keys);   
+        end      
+        
+        function DeleteKeyResponse(obj)
+            try 
+                KbQueueRelease;
+            catch
+                warning('Not using the keyboard')
+            end
         end
-        
-        obj@KeyResponse(valid_indices, varargin);
-        opts = struct('possible_keys', {'a','w','e','f','v','b','h','u','i','l'});
-        opts = CheckInputs(opts, varargin{:});
-        
-        obj.valid_keys = opts.possible_keys(valid_indices);
-        keys = zeros(1, 256);
-        %keys(KbName(obj.valid_keys)) = 1;
-        %KbQueueCreate(-1, keys);
-
-    
     end
-end
 end
