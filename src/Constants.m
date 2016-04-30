e7ec1696498d19904761b3ab047afabf61e919db
Alex Forrence <aforren1@jhu.edu>
2016-04-30 13:25:30 -0400

    Working on constants, standardize parts of keyfeedback

--- src/Constants.m
+++ src/Constants.m
@@ -1,3 +1,11 @@
-function Constants()
+function consts = Constants()
+    consts.timing_tolerance = 0.075;
+    consts.possible_keys =  {{'a','w','e','f','v','b','h','u','i','l'}};
+    consts.force_min = 1;
+    consts.force_max = 5;
+    consts.sampling_freq = 200;
+    consts.reversed = false;
+    consts.big_screen = false;
+    consts.skip_tests = true;
 
 end
