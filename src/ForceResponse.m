65cf8dd63805bd9b39633e24b4cc53bc10807e12
Alex Forrence <aforren1@jhu.edu>
2016-05-02 14:06:50 -0400

    Bound the response rather than rounding it

--- src/ForceResponse.m
+++ src/ForceResponse.m
@@ -79,7 +79,9 @@ classdef ForceResponse
             new_screen_press = new_screen_press - obj.zero_baseline;
             new_screen_press = new_screen_press .* obj.volt_2_newts;
             new_screen_press_scaled = new_screen_press / obj.force_min;
-            updated_screen_press = ifelse(new_screen_press_scaled >= 1, 1, 0);
+            % bound the screen press 
+            update_screen_press(update_screen_press > 1) = 1;
+            update_screen_press(update_screen_press < 0) = 0;
             % make sure to check n and n-1 conditions
             delta_press = updated_screen_press - previous_state;
             press_index = find(delta_press == 1);
