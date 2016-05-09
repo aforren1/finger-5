1e1eb344a5cf562e5a1b55f39f18fcdfc9c2f5ac
Alex Forrence <aforren1@jhu.edu>
2016-05-09 14:49:16 -0400

    Fixed audio test

--- tests/testPsychAudio.m
+++ tests/testPsychAudio.m
@@ -8,7 +8,7 @@ try
    PlayAudio(aud, 1, 0);
    WaitSecs(1);
    FillAudio(aud, 'misc/sounds/smw_coin.wav', 3);
-   PlayAudio(aud, 3, 5);
+   PlayAudio(aud, 3, GetSecs + 3);
    WaitSecs(5);
    CloseAudio(aud);
    success = 1;
