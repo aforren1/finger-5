9569b9af7fe1faeb8a226455a9736382f930b851
Alex Forrence <aforren1@jhu.edu>
2016-05-13 16:52:21 -0400

    Intermediate commit before switching computers

--- main.m
+++ main.m
@@ -11,9 +11,9 @@ function output = main
     end
 
     if strfind(ui.tgt_name, 'tr_')
-
+        % use timed response experiment
     elseif strfind(ui.tgt_name, 'rt_')
-
+        % use serial reaction time experiment
     else
         error('unrecognized experiment');
     end
