d61e601075661946684ca0afbf9101eb875a33ce
Alex Forrence <aforren1@jhu.edu>
2016-05-12 22:23:54 -0400

    Starting to set up experiment again

--- main.m
+++ main.m
@@ -2,6 +2,13 @@ function output = main
 
     % boilerplate initialization
     ui = UserInputs;
+    
+    
+    if ui.keyboard_or_force
+        resp_device = KeyboardResponse(valid_indices);
+    else
+        resp_device = ForceResponse(valid_indices);
+    end
 
     if strfind(ui.tgt_name, 'tr_')
 
