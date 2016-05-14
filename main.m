0dbc62294f8f5d9d9f08a72b2eadae7b11c72e09
Alex Forrence <aforren1@jhu.edu>
2016-05-14 11:36:09 -0400

    Updated main

--- main.m
+++ main.m
@@ -4,11 +4,10 @@ function output = main
 	consts = Constants;
     ui = UserInputs;
     audio = PsychAudio(10);
+    aud_dir = 'misc/sounds/';
 	images = PsychImages(num_images, 'reversed', consts.reversed, 'scale', consts.scale);
 	screen = PsychScreen('reversed', consts.reversed, 'big_screen', consts.big_screen, ...
 	                     'skip_tests', consts.skip_tests);
-	
-	
     
     if ui.keyboard_or_force
         resp_device = KeyboardResponse(valid_indices,...
@@ -25,8 +24,16 @@ function output = main
     end
 
     if strfind(ui.tgt_name, 'tr_')
+        FillAudio(aud, [aud_dir, 'beepTrainFast.wav'], 1);
+        FillAudio(aud, [aud_dir, 'smw_coin.wav'], 2);
         % use timed response experiment
     elseif strfind(ui.tgt_name, 'rt_')
+        FillAudio(aud, [aud_dir, 'beep.wav'], 1);
+        aud_names = dir([aud_dir, 'orch*.wav']);
+        for ii = 1:length(aud_names)
+            FillAudio(aud, [aud_dir, aud_names(ii)], ii);
+        end
+        
         % use serial reaction time experiment
     else
         error('unrecognized experiment');
