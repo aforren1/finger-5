c78955a7ace7d621c1b948c36443572fd2d015ae
Alex Forrence <aforren1@jhu.edu>
2016-06-11 16:02:33 -0400

    Remove library docs to lighten the load, keep (most) parts sequestered to the platformio folder

--- main2.m
+++ main2.m
@@ -7,6 +7,7 @@ function out_data = main2(exp_type, tgt_path, use_serial)
         tgt = ParseTgt(tgt_path);
         exp = Experiment.Factory(exp_type);
         if use_serial
+            system('platformio run --target clean');
             working_serial = ~system('platformio run -e teensy31');
             working_serial = ~system('platformio run -e teensy31 --target upload');
             if ~working_serial
--- src/readme.txt
+++
@@ -1,1 +0,0 @@
-Sources for Arduino/teensy
