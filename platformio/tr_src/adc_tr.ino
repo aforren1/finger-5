2b3244d3d19518994a6d6e15ce86bdbd7def931a
Alex Forrence <aforren1@jhu.edu>
2016-06-13 17:25:26 -0400

    Working on more consistent read

--- platformio/tr_src/adc_tr.ino
+++ platformio/tr_src/adc_tr.ino
@@ -8,11 +8,11 @@ Prototypical arduino/teensy code.
 // only the lines below needs to change
 // first line does which analog channels to read,
 // second line sets the sampling interval (in microseconds)
-const int channel_array[6] = {A0, A1, A2, A3, A4, A5};
-const int period_0 = 2000;
+const unsigned int channel_array[6] = {A0, A1, A2, A3, A4, A5};
+const unsigned long period_0 = 100000;
 
-const int array_size = sizeof(channel_array) / sizeof(int);
-int value_array[array_size];
+const unsigned int array_size = sizeof(channel_array) / sizeof(int);
+unsigned int value_array[array_size];
 
 elapsedMicros current_time;
 IntervalTimer timer_0;
@@ -20,7 +20,7 @@ IntervalTimer timer_0;
 ADC *adc = new ADC();
 
 void setup() {
-  for(int ii = 0; ii < array_size; ii++) {
+  for(unsigned int ii = 0; ii < array_size; ii++) {
     pinMode(channel_array[ii], INPUT);
   }
 
@@ -29,40 +29,47 @@ void setup() {
 
   adc->setReference(ADC_REF_3V3, ADC_0);
   adc->setAveraging(8);
-  adc->setResolution(16);
+  adc->setResolution(12);
   adc->setConversionSpeed(ADC_HIGH_SPEED);
   adc->setSamplingSpeed(ADC_HIGH_SPEED);
-  timer_0.priority(0);
+  timer_0.priority(10);
   timer_0.begin(timerCallback, period_0);
   delay(500);
 }
 
 volatile bool go_flag = false;
+bool go_flag_copy = false;
 
 FASTRUN void timerCallback(void) {
   go_flag = true;
 }
 
 void loop() {
-  bool go_flag_copy = false;
-  go_flag = false;
-  for (int nn = 0; nn < array_size; nn++) {
-    value_array[nn] = adc->analogRead(channel_array[nn]);
-  }
-
-  Serial.print(current_time);
-  Serial.print("\t");
-
-  for (int nn = 0; nn < array_size; nn++) {
-    Serial.print(value_array[nn]);
-    Serial.print("\t");
-  }
-  Serial.print(current_time);
-  Serial.print("\n");
 
   while(!go_flag_copy) {
     noInterrupts();
     go_flag_copy = go_flag;
     interrupts();
   }
+  go_flag_copy = false;
+  go_flag = false;
+
+
+  for (unsigned int nn = 0; nn < array_size; nn++) {
+    value_array[nn] = adc->analogRead(channel_array[nn]);
+  }
+
+  // if s is missing, incomplete line!
+  Serial.print("s");
+  Serial.print("\t");
+  Serial.print(current_time);
+  Serial.print("\t");
+
+  for (unsigned int nn = 0; nn < array_size; nn++) {
+    Serial.print(value_array[nn]);
+    Serial.print("\t");
+  }
+  //Serial.print(current_time);
+  Serial.print("\n");
+
 }
