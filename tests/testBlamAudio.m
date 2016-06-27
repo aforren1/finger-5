6f109ac75dcc2913f5b1919841a99869cdbc498d
Alex Forrence <aforren1@jhu.edu>
2016-06-27 15:56:50 -0400

    Fix tests after syntax change

--- tests/testBlamAudio.m
+++ tests/testBlamAudio.m
@@ -1,13 +1,13 @@
-function success = testPsychAudio
+function success = testBlamAudio
 
 addpath(genpath('matlab'))
 
 try
    aud = BlamAudio;
-   FillAudio(aud, 'misc/sounds/beep.wav', 1, 1);
+   FillAudio(aud, 'misc/sounds/beep.wav', 1, 'left');
    PlayAudio(aud, 1, 0);
    WaitSecs(1);
-   FillAudio(aud, 'misc/sounds/smw_coin.wav', 2, 2);
+   FillAudio(aud, 'misc/sounds/smw_coin.wav', 2, 'right');
    PlayAudio(aud, 2, GetSecs + 3);
    WaitSecs(5);
    % doesn't really work in this example, but does in reality
@@ -16,12 +16,12 @@ try
    PlayAudio(aud, 2, same_time);
    CloseAudio(aud);
    success = 1;
-   
+
 catch
     success = 0;
     try PsychPortAudio('Close');
     catch
         warning('No active audio device')
-    end    
+    end
 
 end
